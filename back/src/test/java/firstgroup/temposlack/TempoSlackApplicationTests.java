ba61625d1915ed6e86878e80b858538aa73c3496
SternHillpocken <nicolas.guerin.pro@laposte.net>
2024-02-04 10:59:44 +0100

    Raw project

---
+++ back/src/test/java/firstgroup/temposlack/TempoSlackApplicationTests.java
@@ -0,0 +1,13 @@
+package firstgroup.temposlack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempoSlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
