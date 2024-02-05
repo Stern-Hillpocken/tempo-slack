f97f6727254272c11dd3007c5f0c2ca7982cec8c
SternHillpocken <nicolas.guerin.pro@laposte.net>
2024-02-05 13:40:03 +0100

    User basic step

--- back/src/main/java/firstgroup/temposlack/controller/UserController.java
+++ back/src/main/java/firstgroup/temposlack/controller/UserController.java
@@ -1,4 +1,26 @@
 package firstgroup.temposlack.controller;
 
+import firstgroup.temposlack.dto.UserPublicDTO;
+import firstgroup.temposlack.dto.UserSignInDTO;
+import firstgroup.temposlack.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserService service;
+
+    @PostMapping()
+    public ResponseEntity<?> add(@RequestBody UserSignInDTO userSignInDTO) {
+        return service.add(userSignInDTO);
+    }
+
+    @GetMapping("/{id}")
+    public UserPublicDTO getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
 }
--- back/src/main/java/firstgroup/temposlack/service/UserService.java
+++ back/src/main/java/firstgroup/temposlack/service/UserService.java
@@ -1,4 +1,39 @@
 package firstgroup.temposlack.service;
 
+import firstgroup.temposlack.dao.UserRepository;
+import firstgroup.temposlack.dto.UserPublicDTO;
+import firstgroup.temposlack.dto.UserSignInDTO;
+import firstgroup.temposlack.mapper.UserPublicMapper;
+import firstgroup.temposlack.mapper.UserSignInMapper;
+import firstgroup.temposlack.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    
+    @Autowired
+    UserRepository repository;
+    
+    public UserPublicDTO getById(Long id) {
+        User user = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("User id: "+id+" not found.")
+        );
+        return UserPublicMapper.userToDTO(user);
+    }
+
+    public User getByPseudo(String pseudo) {
+        User user = repository.findByPseudo(pseudo).orElseThrow(
+                () -> new RuntimeException("Pseudo "+pseudo+" not found.")
+        );
+        return user;
+    }
+
+    public ResponseEntity<?> add(UserSignInDTO userSignInDTO) {
+        User user = UserSignInMapper.signInDTOToUser(userSignInDTO);
+        repository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
