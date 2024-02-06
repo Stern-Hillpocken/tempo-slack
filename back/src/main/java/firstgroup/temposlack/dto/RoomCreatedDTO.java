5917c201fee7a4953c21d6bf63bf6cb56bfd7b4a
Zackiel <yonis.duffau@hotmail.com>
2024-02-06 15:42:32 +0100

    Verifcation User

---
+++ back/src/main/java/firstgroup/temposlack/dto/RoomCreatedDTO.java
@@ -0,0 +1,25 @@
+package firstgroup.temposlack.dto;
+
+public class RoomCreatedDTO {
+
+    private Long id;
+    private String title;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
