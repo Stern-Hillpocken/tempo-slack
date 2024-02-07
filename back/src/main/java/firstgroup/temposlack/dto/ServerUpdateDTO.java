05b6a61ac8f3f3dd1cbd6dd093789fd8782439b2
SternHillpocken <nicolas.guerin.pro@laposte.net>
2024-02-07 15:03:29 +0100

    Refactoring check input and user pseudo password

---
+++ back/src/main/java/firstgroup/temposlack/dto/ServerUpdateDTO.java
@@ -0,0 +1,16 @@
+package firstgroup.temposlack.dto;
+
+public class ServerUpdateDTO {
+
+    private UserPseudoPasswordDTO user;
+
+    private String name;
+
+    public UserPseudoPasswordDTO getUser() {
+        return user;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
