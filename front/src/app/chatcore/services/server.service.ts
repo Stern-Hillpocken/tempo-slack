7bcad826c93c416b6057ef9698b7a7393041bb01
Zackiel <yonis.duffau@hotmail.com>
2024-02-23 19:21:56 +0100

    Add post & button

--- front/src/app/chatcore/services/server.service.ts
+++ front/src/app/chatcore/services/server.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Server } from "src/app/core/models/server.model";
 import { Room } from "src/app/core/models/room.model";
+import { LocalStorageService } from "src/app/shared/local-storage.service";
 
 @Injectable({
   providedIn: "root",
@@ -10,13 +11,19 @@ import { Room } from "src/app/core/models/room.model";
 export class ServerService {
   apiUrl: string = "http://localhost:8080/servers";
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private localStorageService: LocalStorageService) {}
 
-  getServerById(id: number): Observable<Server> {
-    return this.http.get<Server>(`${this.apiUrl}/${id}`);
+  getServerById(idServer: number): Observable<Server> {
+    return this.http.get<Server>(`${this.apiUrl}/${idServer}`);
   }
 
   getRoomInServerById(idServer: number, idRoom: number): Observable<Room> {
     return this.http.get<Room>(`${this.apiUrl}/${idServer}/${idRoom}`);
+  }
+
+  addRoomInServerById(roomTitle: string, idServer: number): Observable<Room> {
+    let dto = { title: roomTitle, user: this.localStorageService.getPseudoPassword() };
+    console.log(dto);
+    return this.http.post<Room>(`${this.apiUrl}/${idServer}`, dto);
   }
 }
