07fd1a10813b54d53369269f8a73b4563645b449
rudwheel <rudy.anne.pro@gmail.com>
2024-02-23 19:32:22 +0100

    add server name component display

--- front/src/app/chatcore/components/server-information/server-name/server-name.component.ts
+++ front/src/app/chatcore/components/server-information/server-name/server-name.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ServerService } from "src/app/chatcore/services/server.service";
+import { Server } from "src/app/core/models/server.model";
 
 @Component({
-  selector: 'app-server-name',
-  templateUrl: './server-name.component.html',
-  styleUrls: ['./server-name.component.scss']
+  selector: "app-server-name",
+  templateUrl: "./server-name.component.html",
+  styleUrls: ["./server-name.component.scss"],
 })
-export class ServerNameComponent {
+export class ServerNameComponent implements OnInit {
+  id!: number;
+  serverName: string = ''; // Nom initial du serveur
 
+  constructor(private serverService: ServerService, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.id = Number(this.activatedRoute.snapshot.paramMap.get("id"));
+    this.serverService.getServerById(this.id).subscribe((server) => {
+      this.serverName = server.name; 
+    });
+  }
+
+ /*  editServerName(newName: string): void {
+    const updatedServer = { name: newName }; 
+    this.serverService.updateServer(this.id, updatedServer).subscribe({
+      next: (updated) => {
+        this.serverName = updated.name; 
+        console.log('Server name updated successfully!');
+      },
+      error: (error) => console.error('There was an error updating the server name', error)
+    });
+  } */
 }
