9497eeb085502fad29b395de217e9e5ac55234e8
Zackiel <yonis.duffau@hotmail.com>
2024-02-26 11:35:30 +0100

    Room Id Fix

---
+++ front/src/app/chatcore/components/server-information/server-members/add-members/add-members.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-members',
+  templateUrl: './add-members.component.html',
+  styleUrls: ['./add-members.component.scss']
+})
+export class AddMembersComponent {
+
+}
